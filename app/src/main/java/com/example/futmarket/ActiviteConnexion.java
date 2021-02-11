a63fb214de4a651284b74eb5f26f097888d95eec
yoperiquoi <yoann_63115@hotmail.fr>
2021-02-11 12:02:12 +0100

    Finalisation des affichage pour l'écran d'acceuil et création des activitées de connection et d'inscription.

---
+++ app/src/main/java/com/example/futmarket/ActiviteConnexion.java
@@ -0,0 +1,14 @@
+package com.example.futmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActiviteConnexion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activite_connexion);
+    }
+}
