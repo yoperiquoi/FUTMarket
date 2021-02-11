727664c0ff0464d59cc2b37a85efe326fdbfb911
yoperiquoi <yoann_63115@hotmail.fr>
2021-02-11 10:48:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/futmarket/model/Joueur.java
@@ -0,0 +1,4 @@
+package com.example.futmarket.model;
+
+public class Joueur {
+}
---
+++ app/src/main/java/com/example/futmarket/view/ActivitePrincipale.java
@@ -0,0 +1,16 @@
+package com.example.futmarket.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.futmarket.R;
+
+public class ActivitePrincipale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activite_principale);
+    }
+}
