bcac5f58d5b9e9659ba8602ab8eef287f46fb47d
mapozdnyak <poz3011@mail.ru>
2021-02-23 12:02:56 +0100

    l'acces et l'affichage des activites connexion et inscription

--- app/src/main/java/com/example/futmarket/view/ActivitePrincipale.java
+++ app/src/main/java/com/example/futmarket/view/ActivitePrincipale.java
@@ -2,8 +2,12 @@ package com.example.futmarket.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.example.futmarket.ActiviteConnexion;
+import com.example.futmarket.ActiviteInscription;
 import com.example.futmarket.R;
 
 public class ActivitePrincipale extends AppCompatActivity {
@@ -12,5 +16,17 @@ public class ActivitePrincipale extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activite_principale);
+
+        allerVersConnection();
+        allerVersInscription();
+    }
+    private void allerVersConnection(){
+        Button connect = findViewById(R.id.connection);
+        connect.setOnClickListener(v-> startActivity(new Intent(ActivitePrincipale.this, ActiviteConnexion.class)));
+    }
+
+    private void allerVersInscription(){
+        Button connect = findViewById(R.id.inscription);
+        connect.setOnClickListener(v-> startActivity(new Intent(ActivitePrincipale.this, ActiviteInscription.class)));
     }
 }
